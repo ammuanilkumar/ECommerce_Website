65d2690549256a4c64c62f3a8236da0509aeb86f
ammuanilkumar <ammupa41@gmail.com>
2024-10-27 17:09:19 +0530

    11th commit

--- client/src/pages/userPages/ProductDetails.jsx
+++ client/src/pages/userPages/ProductDetails.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useParams } from "react-router-dom";
+import { useEffect } from 'react';
+import { axiosInstance } from '../../config/axiosInstance';
 export const ProductDetails = () => {
-  return (
+
+const [ProductDetails, setProductDetails] = useState ({})
+  const {id}= useParams();
+
+  //console.log(id,'====id')
+  const fetchProductDetails = async ()=> {
+    try {
+      const response = await axiosInstance ({
+        url: `/product/details/${id}`,
+        method: "GET",
+        withCredentials: true,
+      });
+      setProductDetails(response?.data?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // useEffect(() => {
+  //   fetchProductDetails();
+  // }, [])
+  useEffect(() => {
+    fetchProductDetails();
+  }, []);
+  return(
     <div>
       ProductDetails
     </div>
