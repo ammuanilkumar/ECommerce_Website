681d3304baf37ce88834d4936001cfc0619c8c8c
ammuanilkumar <ammupa41@gmail.com>
2024-10-28 02:05:13 +0530

    vite@2

--- server/index.js
+++ server/index.js
@@ -10,12 +10,13 @@ import cors from 'cors'
 const app = express()
 app.use(
   cors({
-  origin:"http://localhost:5173",
+  origin:["https://e-commerce-website-client-kappa.vercel.app","http://localhost:5173"],
   credentials: true,
   })
 
-
 );
+
+
 app.use(express.json())
 app.use(cookieParser())
 const port = process.env.PORT || 4001
