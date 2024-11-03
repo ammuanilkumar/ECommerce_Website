353c89eb3feda748cb10b5a9875583a6ca98a545
ammuanilkumar <ammupa41@gmail.com>
2024-11-03 23:46:44 +0530

    3ed update

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -1,5 +1,106 @@
-import { createBrowserRouter } from "react-router-dom";
+// import { createBrowserRouter } from "react-router-dom";
 
+// import { LoginPage } from "./../pages/userPages/LoginPage";
+// import { SignupPage } from "./../pages/userPages/SignupPage";
+// import { RootLayout } from "../layouts/RootLayout";
+// import { AboutUsPage } from "../pages/webpages/AboutUsPage";
+// import { ProductDetails } from "../pages/userPages/ProductDetails";
+// import { ProductPage } from "../pages/userPages/ProductPage";
+// import { CartPage } from "../pages/userPages/CartPage";
+// import { UserLayout } from "../layouts/UserLayout";
+// import { ErrorPage } from "../pages/errorPage/ErrorPage";
+
+// import { HomeRoot } from "../pages/root/HomeRoot";
+// import { UserHome } from "../pages/userPages/UserHome";
+// import { UserProfile } from "../pages/userPages/UserProfile";
+// import { OrderDetails } from "../pages/userPages/OrderDetails";
+// import { UserAuth } from "./protectedRoutes/UserAuth";
+// // import { AdminAuth } from "./protectedRoutes/AdminAuth";
+//  import {  AdminLoginPage  } from  "./../pages/adminPages/AdminLogin";
+
+
+// // import { Offers } from "../pages/userPages/Offers"; // Added this import
+
+// export const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     errorElement: <ErrorPage />,
+//     element: <RootLayout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <HomeRoot />,
+//       },
+//       {
+//         path: "about",
+//         element: <AboutUsPage />,
+//       },
+//       {
+//         path: "login",
+//         element: <LoginPage />,
+//       },
+//       {
+//         path: "signup",
+//         element: <SignupPage />,
+//       },
+//     ],
+//   },
+//   {
+//     path: "user",
+
+//     element: (
+//       <UserAuth>
+//         <UserLayout />
+//       </UserAuth>
+//     ),
+//     children: [
+//       {
+//         path: "home",
+//         element: <UserHome />,
+//       },
+//       {
+//         path: "about",
+//         element: <AboutUsPage />,
+//       },
+//       {
+//         path: "profile",
+//         element: <UserProfile />,
+//       },
+//       {
+//         path: "orderdetails",
+//         element: <OrderDetails />,
+//       },
+//       {
+//         path: "product",
+//         element: <ProductPage />,
+//       },
+//       {
+//         path: "product-details/:id",
+//         element: <ProductDetails />,
+//       },
+//       {
+//         path: "cart",
+//         element: <CartPage />,
+//       },
+//     ],
+//     // nathaa error anuu  slack aych dk verum 
+//   },
+//   // {
+//   //   path: "adminlogin",
+//   //   // element: <AdminLoginPage />,
+//   // },
+//   // path: "admin",
+//   // element: (
+//   //   <AdminAuth>
+//   //     <AdminLayout />
+//   //   </AdminAuth>
+//   // ),
+
+//   // evidaa   nee bakkii set akkanamm emtoo  ok
+// ]);
+
+
+import { createBrowserRouter } from "react-router-dom";
 import { LoginPage } from "./../pages/userPages/LoginPage";
 import { SignupPage } from "./../pages/userPages/SignupPage";
 import { RootLayout } from "../layouts/RootLayout";
@@ -9,17 +110,12 @@ import { ProductPage } from "../pages/userPages/ProductPage";
 import { CartPage } from "../pages/userPages/CartPage";
 import { UserLayout } from "../layouts/UserLayout";
 import { ErrorPage } from "../pages/errorPage/ErrorPage";
-
 import { HomeRoot } from "../pages/root/HomeRoot";
 import { UserHome } from "../pages/userPages/UserHome";
 import { UserProfile } from "../pages/userPages/UserProfile";
 import { OrderDetails } from "../pages/userPages/OrderDetails";
 import { UserAuth } from "./protectedRoutes/UserAuth";
-// import { AdminAuth } from "./protectedRoutes/AdminAuth";
- import {  AdminLoginPage  } from  "./../pages/adminPages/AdminLogin";
-
-
-// import { Offers } from "../pages/userPages/Offers"; // Added this import
+import { AdminLoginPage } from "./../pages/adminPages/AdminLogin";
 
 export const router = createBrowserRouter([
   {
@@ -28,7 +124,7 @@ export const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       {
-        path: "",
+        path: "/",
         element: <HomeRoot />,
       },
       {
@@ -47,7 +143,6 @@ export const router = createBrowserRouter([
   },
   {
     path: "user",
-
     element: (
       <UserAuth>
         <UserLayout />
@@ -83,18 +178,9 @@ export const router = createBrowserRouter([
         element: <CartPage />,
       },
     ],
-    // nathaa error anuu  slack aych dk verum 
   },
-  // {
-  //   path: "adminlogin",
-  //   // element: <AdminLoginPage />,
-  // },
-  // path: "admin",
-  // element: (
-  //   <AdminAuth>
-  //     <AdminLayout />
-  //   </AdminAuth>
-  // ),
-
-  // evidaa   nee bakkii set akkanamm emtoo  ok
+  {
+    path: "adminlogin",
+    element: <AdminLoginPage />,
+  },
 ]);
