ed613f721a8350c98a6e5e496f790dd498d51e5b
ammuanilkumar <ammupa41@gmail.com>
2024-11-04 00:14:19 +0530

    4th update  router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,12 @@
-
-
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Router.jsx";
-import toast, { Toaster } from 'react-hot-toast';
-//import toast from "react-hot-toast";
-//
+import toast, { Toaster } from "react-hot-toast";
+
 function App() {
   return (
     <>
-   
       <RouterProvider router={router} />
       <Toaster />
-
     </>
   );
 }
--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -1,105 +1,3 @@
-// import { createBrowserRouter } from "react-router-dom";
-
-// import { LoginPage } from "./../pages/userPages/LoginPage";
-// import { SignupPage } from "./../pages/userPages/SignupPage";
-// import { RootLayout } from "../layouts/RootLayout";
-// import { AboutUsPage } from "../pages/webpages/AboutUsPage";
-// import { ProductDetails } from "../pages/userPages/ProductDetails";
-// import { ProductPage } from "../pages/userPages/ProductPage";
-// import { CartPage } from "../pages/userPages/CartPage";
-// import { UserLayout } from "../layouts/UserLayout";
-// import { ErrorPage } from "../pages/errorPage/ErrorPage";
-
-// import { HomeRoot } from "../pages/root/HomeRoot";
-// import { UserHome } from "../pages/userPages/UserHome";
-// import { UserProfile } from "../pages/userPages/UserProfile";
-// import { OrderDetails } from "../pages/userPages/OrderDetails";
-// import { UserAuth } from "./protectedRoutes/UserAuth";
-// // import { AdminAuth } from "./protectedRoutes/AdminAuth";
-//  import {  AdminLoginPage  } from  "./../pages/adminPages/AdminLogin";
-
-
-// // import { Offers } from "../pages/userPages/Offers"; // Added this import
-
-// export const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     errorElement: <ErrorPage />,
-//     element: <RootLayout />,
-//     children: [
-//       {
-//         path: "",
-//         element: <HomeRoot />,
-//       },
-//       {
-//         path: "about",
-//         element: <AboutUsPage />,
-//       },
-//       {
-//         path: "login",
-//         element: <LoginPage />,
-//       },
-//       {
-//         path: "signup",
-//         element: <SignupPage />,
-//       },
-//     ],
-//   },
-//   {
-//     path: "user",
-
-//     element: (
-//       <UserAuth>
-//         <UserLayout />
-//       </UserAuth>
-//     ),
-//     children: [
-//       {
-//         path: "home",
-//         element: <UserHome />,
-//       },
-//       {
-//         path: "about",
-//         element: <AboutUsPage />,
-//       },
-//       {
-//         path: "profile",
-//         element: <UserProfile />,
-//       },
-//       {
-//         path: "orderdetails",
-//         element: <OrderDetails />,
-//       },
-//       {
-//         path: "product",
-//         element: <ProductPage />,
-//       },
-//       {
-//         path: "product-details/:id",
-//         element: <ProductDetails />,
-//       },
-//       {
-//         path: "cart",
-//         element: <CartPage />,
-//       },
-//     ],
-//     // nathaa error anuu  slack aych dk verum 
-//   },
-//   // {
-//   //   path: "adminlogin",
-//   //   // element: <AdminLoginPage />,
-//   // },
-//   // path: "admin",
-//   // element: (
-//   //   <AdminAuth>
-//   //     <AdminLayout />
-//   //   </AdminAuth>
-//   // ),
-
-//   // evidaa   nee bakkii set akkanamm emtoo  ok
-// ]);
-
-
 import { createBrowserRouter } from "react-router-dom";
 import { LoginPage } from "./../pages/userPages/LoginPage";
 import { SignupPage } from "./../pages/userPages/SignupPage";
@@ -120,11 +18,11 @@ import { AdminLoginPage } from "./../pages/adminPages/AdminLogin";
 export const router = createBrowserRouter([
   {
     path: "/",
-    errorElement: <ErrorPage />,
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
-        path: "/",
+        path: "",
         element: <HomeRoot />,
       },
       {
